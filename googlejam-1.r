1ad14acafb0a6502ca199f1686b1379be580f769
pepdiz <pepdiz@users.noreply.github.com>
2016-04-11 20:45:15 +0200

    first version googlejam 2016 Counting Sheep
    a reboling to solve first challenge of google jam 2016, the "Counting sheep" problem

---
+++ googlejam-1.r
@@ -0,0 +1,38 @@
+rebol [
+	Author: [ "Pep Diz" eukelade@gmail.com ]
+	Title: "Counting Sheep"
+    File:  %googlejam-1.r
+    Version: 1.0
+    Date:       22-Mar-2007
+    Purpose: { This script solves the googlejam 2016 challenge number 1 Counting Sheep 
+			Reads input from file input.txt and writes output to screen and also to file output.txt }
+    ]
+
+init: [ #"0" false #"1" false #"2" false #"3" false #"4" false #"5" false #"6" false #"7" false #"8" false #"9" false ]
+
+write %output.txt ""
+entrada: read/lines %input.txt
+
+nc: to-integer first entrada
+
+for c 1 nc 1 [
+	r: none
+	p: ""
+	n: entrada/(c + 1)
+	d: to-integer n
+		
+	dixitos: copy init
+	until [
+		foreach i n [ dixitos/:i: true ]
+		p: copy n
+		n: to-string (d + to-integer n)
+		if p = n [r: "INSOMNIA" break]	
+		all extract/index dixitos 2 2
+	]
+
+	if r = none [r: p]
+
+	l: join "Case #" [c ": " r ]
+	print l
+	write/lines/append %output.txt reduce [l]
+]
