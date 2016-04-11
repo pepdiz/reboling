3af8b96e65eb3fe73bb14dde83f4326bba2fabd2
pepdiz <pepdiz@users.noreply.github.com>
2016-04-11 22:06:12 +0200

    read from standard input and optimizing

--- googlejam-1.r
+++ googlejam-1.r
@@ -5,27 +5,26 @@ rebol [
     Version: 1.0
     Date: 10-04-2016
     Purpose: { This script solves the googlejam 2016 challenge number 1 Counting Sheep 
-		Reads input from file input.txt and writes to standard output }
+               Reads input from standard input and writes output to standard output }
     ]
 
 init: [ #"0" false #"1" false #"2" false #"3" false #"4" false #"5" false #"6" false #"7" false #"8" false #"9" false ]
 
-entrada: read/lines %input.txt
-
-for c 1 to-integer entrada/1 1 [
-	r: none
-	p: ""
+entrada: to-block read system/ports/input
+ 
+for c 1 entrada/1 1 [
+	p: none
 	n: entrada/(c + 1)
-	d: to-integer n
+	d: n
 		
 	dixitos: copy init
 	until [
-		foreach i n [ dixitos/:i: true ]
-		p: copy n
-		n: to-string (d + to-integer n)
-		if p = n [r: "INSOMNIA" break]	
+		foreach i to-string n [ dixitos/:i: true ]
+		p: n
+		n: d + n
+		if p = n [p: "INSOMNIA" break]	
 		all extract/index dixitos 2 2
 	]
 
-	print [join "Case #" [c ":"] any [r p]]
+	print [join "Case #" [c ":"] p]
 ]
